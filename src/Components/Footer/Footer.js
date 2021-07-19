4b040f63184287081fe93d8c58b1627332707ccd
Gabriel <gabrielratz84@gmail.com>
2021-07-19 11:45:27 +0200

    footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -12,34 +12,41 @@ function Footer() {
           <LogoTextLower>Peru</LogoTextLower>
         </ShopNameDiv>
       </LogoDiv>
-      <div>
+      <ColumnTextDiv>
         <h1>Company</h1>
         <p>Home</p>
         <p>About us</p>
-        <p>Contact us</p>
-      </div>
-      <div>
+        <a href="mailto:abc@example.com">Contact us</a>
+      </ColumnTextDiv>
+      <ColumnTextDiv>
         <h1>Contact</h1>
-        <p>+6605685648</p>
-        <p>pati@gmx.at</p>
-      </div>
-      <div>
+        <p>Tel: +6605685648</p>
+        <p>Email: pati@gmx.at</p>
+      </ColumnTextDiv>
+      <ColumnTextDiv>
         <h1>Adress</h1>
         <p>Patricia Amedee </p>
         <p>Vienna 1020</p>
         <p>Austria</p>
-      </div>
+      </ColumnTextDiv>
     </MainDiv>
   );
 }
 
 export default Footer;
 
+const ColumnTextDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  height: 100%;
+`;
+
 const MainDiv = styled.div`
   display: flex;
   justify-content: space-around;
-
   width: 100%;
+  padding: 2em;
 `;
 
 const LogoDiv = styled.div`
